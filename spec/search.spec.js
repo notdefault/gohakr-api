db67d68fcf03e12a40b9ff5566647c96a1998f4b
Danny Grove <danny@drgrovellc.com>
2014-04-26 10:36:48 -0400

    moves libs to lib. Adds jasmine

---
+++ spec/search.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('search', function() {
+
+  var search;
+  beforeEach(function() {
+    search = require('../lib/search');
+  });
+
+  describe('Your first test', function() {
+
+    it('does something meaningful', function() {
+      expect(search.prop1).toEqual('prop1');
+    });
+
+  });
+
+});
